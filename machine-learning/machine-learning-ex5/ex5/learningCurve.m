337b503981aba2882e553403f3ed58e67c36d27d
Tzachi Ezra Torf Fulton <eztzachi@gmail.com>
2015-06-02 15:32:38 -0700

    ex 5

--- machine-learning/machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning/machine-learning-ex5/ex5/learningCurve.m
@@ -53,12 +53,14 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
-
-
-
-
-
-
+%x = [ones(m, 1) X];
+%xval = [ones(size(Xval, 1), 1) Xval];
+for i = 1:m
+    theta = trainLinearReg(X(1:i, :), y(1:i), lambda);
+    error_train(i) = linearRegCostFunction(X(1:i, :), y(1:i),theta, 0);
+    error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+end
+ 
 % -------------------------------------------------------------
 
 % =========================================================================
